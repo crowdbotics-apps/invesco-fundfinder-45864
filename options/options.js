6bf3158bc7b881b410fcdb66388cb6385d7ef4d5
Crowdbotics <team@crowdbotics.com>
2024-02-05 04:35:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "invesco-fundfinder-45864",
+  url: "https://invesco-fundfinder-45864.botics.co",
+  api: "https://invesco-fundfinder-45864.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
